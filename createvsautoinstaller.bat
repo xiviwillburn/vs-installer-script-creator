c51ef05c6296a7e2a25e936a559caea995e8523d
xiviwillburn <45106237+xiviwillburn@users.noreply.github.com>
2023-12-01 19:12:48 +0100

    Update createvsautoinstaller.bat
    Added check for .vsconfig file

--- createvsautoinstaller.bat
+++ createvsautoinstaller.bat
@@ -1,5 +1,12 @@
 @echo off
 rem create an vsautoinstaller
+set configFile=".vsconfig"
+if exist ".vsconfig" (
+    GoTo CREATE_FILE
+) else (
+    GoTo ERROR
+)
+:CREATE_FILE
 set myfile=vsautoinstaller.bat
 echo @echo off>%myfile%
 echo rem name of the temp file to store the config>>%myfile%
@@ -8,7 +15,7 @@ echo rem path to the config tempfile>>%myfile%
 echo SET configPath=%%~dp0%%configFileName%%>>%myfile%
 echo rem data to be added to the config file>>%myfile%
 echo | set /p =SET configFileData=>>%myfile%
-for /F "tokens=* delims=-" %%i in (.vsconfig) do (
+for /F "tokens=* delims=-" %%i in (configFile) do (
     echo | set /p =%%i >>%myfile%
 )
 echo rem echo %%configPath%%>>%myfile%
@@ -20,3 +27,8 @@ echo rem install vstudio with the provided config file>>%myfile%
 echo winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config %%configPath%%" --accept-package-agreements>>%myfile%
 echo rem delete temp file>>%myfile%
 echo del /Q %%configPath%%>>%myfile%
+GoTo END
+:ERROR
+echo %configFile% not found 
+pause
+:END
