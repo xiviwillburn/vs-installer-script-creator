4d263c5296afa7ac1da7e2026ae5a74b0cd15450
xiviwillburn <45106237+xiviwillburn@users.noreply.github.com>
2023-12-03 01:07:56 +0100

    changed how to store vsconfig file to store as base64

--- createvsautoinstaller.bat
+++ createvsautoinstaller.bat
@@ -1,6 +1,5 @@
 @echo off
 rem create an vsautoinstaller
-set configFile=.vsconfig
 if exist .vsconfig (
     GoTo CREATE_FILE
 ) else (
@@ -8,25 +7,31 @@ if exist .vsconfig (
 )
 :CREATE_FILE
 set myfile=vsautoinstaller.bat
+certutil -encode -f ".vsconfig" file.tmp
+setlocal enabledelayedexpansion
+
 echo @echo off>%myfile%
 echo rem name of the temp file to store the config>>%myfile%
-echo SET configFileName=a.vsconfig>>%myfile%
+echo SET configFile64=a.vsconfig>>%myfile%
+echo SET configFile=.vsconfig >>%myfile%
 echo rem path to the config tempfile>>%myfile%
-echo SET configPath=%%~dp0%%configFileName%%>>%myfile%
+echo SET configPath=%%~dp0%%configFile64%%>>%myfile%
 echo rem data to be added to the config file>>%myfile%
 echo | set /p =SET configFileData=>>%myfile%
-for /F "tokens=* delims=-" %%i in (.vsconfig) do (
-    echo | set /p =%%i >>%myfile%
+for /f "usebackq delims=" %%I in ("file.tmp") do (
+echo | set /p=%%I>>%myfile%
 )
-echo rem echo %%configPath%%>>%myfile%
-echo rem create tempfile>>%myfile%
+echo: >> %myfile%
+echo rem decode data>>%myfile%
 echo wininit^>%%configPath%%>>%myfile%
-echo rem write data to the temp file>>%myfile%
-echo echo %%configFileData%% ^> %%configPath%%>>%myfile%
+echo echo %%configFileData%%^>%%configPath%%>>%myfile%
+echo certutil -decode %%configPath%% %%configFile%%>>%myfile%
 echo rem install vstudio with the provided config file>>%myfile%
-echo winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config %%configPath%%" --accept-package-agreements --accept-source-agreements>>%myfile%
+echo winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config %%configFile%%" --accept-package-agreements --accept-source-agreements>>%myfile%
 echo rem delete temp file>>%myfile%
 echo del /Q %%configPath%%>>%myfile%
+echo del /Q %%configFile%%>>%myfile%
+
 GoTo END
 :ERROR
 echo %configFile% not found 
